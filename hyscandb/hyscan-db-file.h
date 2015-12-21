3e49ae583926755439891233821e481ad61cccda
Andrei Fadeev <andrei@webcontrol.ru>
2015-12-21 13:59:13 +0300

    Переформатирование кода.

--- hyscandb/hyscan-db-file.h
+++ hyscandb/hyscan-db-file.h
@@ -20,12 +20,12 @@
 
 G_BEGIN_DECLS
 
-#define HYSCAN_TYPE_DB_FILE               (hyscan_db_file_get_type ())
-#define HYSCAN_DB_FILE( obj )             (G_TYPE_CHECK_INSTANCE_CAST ((obj), HYSCAN_TYPE_DB_FILE, HyScanDBFile))
-#define HYSCAN_IS_DB_FILE( obj )          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), HYSCAN_TYPE_DB_FILE))
-#define HYSCAN_DB_FILE_CLASS( klass )     (G_TYPE_CHECK_CLASS_CAST ((klass), HYSCAN_TYPE_DB_FILE, HyScanDBFileClass))
-#define HYSCAN_IS_DB_FILE_CLASS( klass )  (G_TYPE_CHECK_CLASS_TYPE ((klass), HYSCAN_TYPE_DB_FILE))
-#define HYSCAN_DB_FILE_GET_CLASS( obj )   (G_TYPE_INSTANCE_GET_CLASS ((obj), HYSCAN_TYPE_DB_FILE, HyScanDBFileClass))
+#define HYSCAN_TYPE_DB_FILE             (hyscan_db_file_get_type ())
+#define HYSCAN_DB_FILE(obj)             (G_TYPE_CHECK_INSTANCE_CAST ((obj), HYSCAN_TYPE_DB_FILE, HyScanDBFile))
+#define HYSCAN_IS_DB_FILE(obj)          (G_TYPE_CHECK_INSTANCE_TYPE ((obj), HYSCAN_TYPE_DB_FILE))
+#define HYSCAN_DB_FILE_CLASS(klass)     (G_TYPE_CHECK_CLASS_CAST ((klass), HYSCAN_TYPE_DB_FILE, HyScanDBFileClass))
+#define HYSCAN_IS_DB_FILE_CLASS(klass)  (G_TYPE_CHECK_CLASS_TYPE ((klass), HYSCAN_TYPE_DB_FILE))
+#define HYSCAN_DB_FILE_GET_CLASS(obj)   (G_TYPE_INSTANCE_GET_CLASS ((obj), HYSCAN_TYPE_DB_FILE, HyScanDBFileClass))
 
 typedef struct _HyScanDBFile HyScanDBFile;
 typedef struct _HyScanDBFileClass HyScanDBFileClass;
@@ -35,7 +35,7 @@ struct _HyScanDBFileClass
   GObjectClass parent_class;
 };
 
-GType hyscan_db_file_get_type( void );
+GType hyscan_db_file_get_type (void);
 
 G_END_DECLS
 
